060976a97f9e02af82afbeb63e0808c99461efec
Yichuan Lin Lee <lyichuan@gmail.com>
2015-08-03 20:05:33 +0200

    Crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -55,3 +55,23 @@ exports.answer = function(req, res){
         res.render('quizes/answer', { quiz: req.quiz, respuesta: 'Incorrecto'});
     }
 };
+
+// GET /quizes/new
+exports.new = function(req, res) {
+	var quiz = models.Quiz.build(
+		// crea objeto quiz
+		{pregunta: 'Pregunta', respuesta: 'Respuesta'}
+	);
+
+	res.render('quizes/new', {quiz: quiz} );
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build(req.body.quiz);
+
+	//guarda en BD los campos pregunta y respuesta de quiz
+	quiz.save({fields: ['pregunta', 'respuesta']}).then(function(){
+		res.redirect('/quizes');
+	}); // Redireccion HTTP (URL relativo) lista de preguntas
+};
